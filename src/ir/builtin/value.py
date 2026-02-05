27c8653bb59612ca41cb612760bfeb7e582dd335
Fangtangtang <2167295472@qq.com>
2026-02-05 15:55:57 -0500

    [feat] add simple casting rules

--- src/ir/builtin/value.py
+++ src/ir/builtin/value.py
@@ -4,7 +4,7 @@
 import ast
 from .handler import BuiltinHandler, register_builtin_handler
 from allo._mlir.dialects import arith as arith_d, allo as allo_d, linalg as linalg_d
-from allo._mlir.ir import IntegerType
+from allo._mlir.ir import IntegerType, UnitAttr
 from allo.ir.types import (
     AlloType,
     Index,
@@ -63,6 +63,13 @@ class CastHandler(BuiltinHandler):
             (Fixed, UFixed): "cast_fixed_to_fixed",
             (UFixed, Fixed): "cast_fixed_to_fixed",
             (UFixed, UFixed): "cast_fixed_to_fixed",
+            # UInt/Int -> UInt/Int
+            (Int, Int): "cast_int",
+            (UInt, UInt): "cast_int",
+            (Int, UInt): "cast_int",
+            (UInt, Int): "cast_int",
+            # Float -> Float
+            (Float, Float): "cast_float",
             # Float -> Index
             (Float, Index): "cast_float_to_index",
             # Index -> Float
@@ -75,29 +82,9 @@ class CastHandler(BuiltinHandler):
             (UFixed, Index): "cast_fixed_to_index",
         }
         src_type, res_type = args[0], args[1]
-        handler_name = None
         if (type(src_type), type(res_type)) in cast_map:
             handler_name = cast_map[(type(src_type), type(res_type))]
-        elif isinstance(src_type, (Int, UInt)) and isinstance(res_type, (Int, UInt)):
-            if src_type.bits > res_type.bits:
-                handler_name = "cast_int_trunc"
-            elif src_type.bits < res_type.bits:
-                if isinstance(res_type, UInt):
-                    handler_name = "cast_int_zext"
-                else:
-                    handler_name = "cast_int_sext"
-            else:  # same bits, signed <-> unsigned
-                handler_name = "cast_int_signedness"
-        elif isinstance(src_type, Float) and isinstance(res_type, Float):
-            if src_type.bits > res_type.bits:
-                handler_name = "cast_float_trunc"
-            elif src_type.bits < res_type.bits:
-                handler_name = "cast_float_ext"
-            else:  # same bits, same float type
-                handler_name = "cast_float_identity"
         else:
-            raise NotImplementedError
-        if handler_name is None:
             raise TypeError(f"Invalid casting. src: {src_type}, dst: {res_type}")
 
         return res_type, src_type, handler_name
@@ -113,178 +100,216 @@ class CastHandler(BuiltinHandler):
 class IndexCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.IndexCastOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            op = arith_d.IndexCastOp(mlir_type, val, ip=self.builder.get_ip())
+            if is_unsigned:
+                op.attributes["unsigned"] = UnitAttr.get()
+            return op
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_si_to_fp")
 class SIToFPCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.SIToFPOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, _ = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            return arith_d.SIToFPOp(mlir_type, val, ip=self.builder.get_ip())
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_ui_to_fp")
 class UIToFPCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.UIToFPOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, _ = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            return arith_d.UIToFPOp(mlir_type, val, ip=self.builder.get_ip())
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_fp_to_si")
 class FPToSICastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.FPToSIOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, _ = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            return arith_d.FPToSIOp(mlir_type, val, ip=self.builder.get_ip())
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_fp_to_ui")
 class FPToUICastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.FPToUIOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, _ = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            return arith_d.FPToUIOp(mlir_type, val, ip=self.builder.get_ip())
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_float_to_fixed")
 class FloatToFixedCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return allo_d.FloatToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            op = allo_d.FloatToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+            if is_unsigned:
+                op.attributes["unsigned"] = UnitAttr.get()
+            return op
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_fixed_to_float")
 class FixedToFloatCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return allo_d.FixedToFloatOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, _ = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            return allo_d.FixedToFloatOp(mlir_type, val, ip=self.builder.get_ip())
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_fixed_to_int")
 class FixedToIntCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return allo_d.FixedToIntOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            op = allo_d.FixedToIntOp(mlir_type, val, ip=self.builder.get_ip())
+            if is_unsigned:
+                op.attributes["unsigned"] = UnitAttr.get()
+            return op
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_int_to_fixed")
 class IntToFixedCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return allo_d.IntToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            op = allo_d.IntToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+            if is_unsigned:
+                op.attributes["unsigned"] = UnitAttr.get()
+            return op
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_fixed_to_fixed")
 class FixedToFixedCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return allo_d.FixedToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+        mlir_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(mlir_type, "shape", [])) == 0:
+            # scalar
+            op = allo_d.FixedToFixedOp(mlir_type, val, ip=self.builder.get_ip())
+            if is_unsigned:
+                op.attributes["unsigned"] = UnitAttr.get()
+            return op
+        raise NotImplementedError
 
 
 @register_builtin_handler("cast_int")
 class IntCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        # Note: redundant logic here if we rely on infer, but keeping it for safety if CastHandler.infer falls back to "cast_int"
-        src_type = val.type
-        dst_type, is_signed = self.get_result_type(node)
-        src_width = src_type.width
-        dst_width = dst_type.width
-
-        if src_width > dst_width:
-            return arith_d.TruncIOp(dst_type, val, ip=self.builder.get_ip())
-        elif src_width < dst_width:
-            # Default to Signed Extension if unknown
-            return arith_d.ExtSIOp(dst_type, val, ip=self.builder.get_ip())
+        dst_type, is_unsigned = self.get_result_type(node)
+        if len(getattr(dst_type, "shape", [])) == 0:
+            # scalar
+            src_width = val.type.width
+            dst_width = dst_type.width
+            if src_width > dst_width:
+                op = arith_d.TruncIOp(dst_type, val, ip=self.builder.get_ip())
+                if is_unsigned:
+                    op.attributes["unsigned"] = UnitAttr.get()
+                return op
+            elif src_width < dst_width:
+                if is_unsigned:
+                    op = arith_d.ExtUIOp(dst_type, val, ip=self.builder.get_ip())
+                    op.attributes["unsigned"] = UnitAttr.get()
+                    return op
+                else:
+                    return arith_d.ExtSIOp(dst_type, val, ip=self.builder.get_ip())
+            else:
+                return val
         else:
-            return val
-
-
-@register_builtin_handler("cast_int_sext")
-class IntSExtCastHandler(CastHandler):
-    def build(self, node: ast.Call, *args):
-        val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.ExtSIOp(mlir_type, val, ip=self.builder.get_ip())
-
-
-@register_builtin_handler("cast_int_zext")
-class IntZExtCastHandler(CastHandler):
-    def build(self, node: ast.Call, *args):
-        val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.ExtUIOp(mlir_type, val, ip=self.builder.get_ip())
-
-
-@register_builtin_handler("cast_int_trunc")
-class IntTruncCastHandler(CastHandler):
-    def build(self, node: ast.Call, *args):
-        val = self.get_operand(node)
-        mlir_type, is_signed = self.get_result_type(node)
-        return arith_d.TruncIOp(mlir_type, val, ip=self.builder.get_ip())
+            raise NotImplementedError
 
 
 @register_builtin_handler("cast_float")
 class FloatCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
         val = self.get_operand(node)
-        dst_type, is_signed = self.get_result_type(node)
-        src_width = val.type.width
-        dst_width = dst_type.width
-        if src_width > dst_width:
-            return arith_d.TruncFOp(dst_type, val, ip=self.builder.get_ip())
-        elif src_width < dst_width:
-            return arith_d.ExtFOp(dst_type, val, ip=self.builder.get_ip())
-        return val
+        dst_type, _ = self.get_result_type(node)
+        if len(getattr(dst_type, "shape", [])) == 0:
+            # scalar
+            src_width = val.type.width
+            dst_width = dst_type.width
+            if src_width > dst_width:
+                return arith_d.TruncFOp(dst_type, val, ip=self.builder.get_ip())
+            elif src_width < dst_width:
+                return arith_d.ExtFOp(dst_type, val, ip=self.builder.get_ip())
+            else:
+                return val
+        else:
+            raise NotImplementedError
 
 
 @register_builtin_handler("cast_float_to_index")
 class FloatToIndexCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
+        raise NotImplementedError
         val = self.get_operand(node)
         # FP -> UI -> Index
         inter_type = IntegerType.get_signless(32)
         op = arith_d.FPToUIOp(inter_type, val, ip=self.builder.get_ip())
-        mlir_type, is_signed = self.get_result_type(node)
+        mlir_type, is_unsigned = self.get_result_type(node)
         return arith_d.IndexCastOp(mlir_type, op.result, ip=self.builder.get_ip())
 
 
 @register_builtin_handler("cast_index_to_float")
 class IndexToFloatCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
+        raise NotImplementedError
         val = self.get_operand(node)
         # Index -> SI -> FP
         inter_type = IntegerType.get_signless(32)
         op = arith_d.IndexCastOp(inter_type, val, ip=self.builder.get_ip())
-        mlir_type, is_signed = self.get_result_type(node)
+        mlir_type, is_unsigned = self.get_result_type(node)
         return arith_d.SIToFPOp(mlir_type, op.result, ip=self.builder.get_ip())
 
 
 @register_builtin_handler("cast_index_to_fixed")
 class IndexToFixedCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
+        raise NotImplementedError
         val = self.get_operand(node)
         inter_type = IntegerType.get_signless(32)
         op = arith_d.IndexCastOp(inter_type, val, ip=self.builder.get_ip())
-        mlir_type, is_signed = self.get_result_type(node)
+        mlir_type, is_unsigned = self.get_result_type(node)
         return allo_d.IntToFixedOp(mlir_type, op.result, ip=self.builder.get_ip())
 
 
 @register_builtin_handler("cast_fixed_to_index")
 class FixedToIndexCastHandler(CastHandler):
     def build(self, node: ast.Call, *args):
+        raise NotImplementedError
         val = self.get_operand(node)
         inter_type = IntegerType.get_signless(32)
         op = allo_d.FixedToIntOp(inter_type, val, ip=self.builder.get_ip())
-        mlir_type, is_signed = self.get_result_type(node)
+        mlir_type, is_unsigned = self.get_result_type(node)
         return arith_d.IndexCastOp(mlir_type, op.result, ip=self.builder.get_ip())
 
 

548cd54b38a493b8c48efd6a223bced91d462050
Fangtangtang <2167295472@qq.com>
2026-02-26 00:38:24 -0500

    [test] update test case

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -13,7 +13,7 @@ S = Layout.Shard
 R = Layout.Replicate
 
 
-def test_get_wid_1D():
+def test_get_wid_1D_1():
     @spmw.unit()
     def top(A: int32[1024], B: int32[1024]):
         @spmw.work(mapping=[1])
@@ -32,22 +32,32 @@ def test_get_wid_1D():
             np.testing.assert_allclose(A + 1, B)
             print("Passed!")
 
-    # vlen = 1024
-    # P = 4
-    # tlen = vlen // P
 
-    # @spmw.unit()
-    # def top(A: int32[vlen], B: int32[vlen]):
-    #     @spmw.work(mapping=[P])
-    #     def core():
-    #         pi: ConstExpr[index] = spmw.get_wid()
-    #         if pi > 1:
-    #             B[tlen * pi:tlen * (pi + 1)] = A[tlen * pi:tlen * (pi + 1)] + pi
-    #         else:
-    #             B[tlen * pi:tlen * (pi + 1)] = A[tlen * pi:tlen * (pi + 1)] - pi
+def test_get_wid_1D_2():
+    vlen = 1024
+    P = 4
+    tlen = vlen // P
 
-    s = to_hls(top)
+    @spmw.unit()
+    def top(A: int32[vlen], B: int32[vlen]):
+        @spmw.work(mapping=[P])
+        def core():
+            pi: ConstExpr[index] = spmw.get_wid()
+            for i in range(tlen * pi, tlen * (pi + 1)):
+                B[i] = A[i] + 1
+
+    if hls.is_available("vitis_hls"):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            mod = to_hls(top, project=tmpdir)
+            A = np.random.rand(
+                1024,
+            ).astype(np.int32)
+            B = np.zeros((1024,), dtype=np.int32)
+            mod(A, B)
+            np.testing.assert_allclose(A + 1, B)
+            print("Passed!")
 
 
 if __name__ == "__main__":
-    test_get_wid_1D()
+    test_get_wid_1D_1()
+    test_get_wid_1D_2()

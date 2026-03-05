33b7cb88cdf0d18909edfe3adef66e21014d497e
Fangtangtang <2167295472@qq.com>
2026-03-04 23:48:51 -0500

    ckpt

--- src/passes/instantiate.py
+++ src/passes/instantiate.py
@@ -2,9 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
-from collections import defaultdict
 from .utils import (
-    parse_spmw_module,
     parse_hierarchical_spmw_module,
     is_resource,
     collect_symbol_refs_in_function,
@@ -24,173 +22,15 @@ from allo._mlir.ir import (
     StringAttr,
     FlatSymbolRefAttr,
     UnitAttr,
-    BlockArgument,
 )
 from allo._mlir.dialects import (
     allo as allo_d,
     arith as arith_d,
     func as func_d,
-    memref as memref_d,
 )
 
 
 def instantiate_for_hls(module, top_name):
-    """
-    [NOTE]: hls backend do not support sharding for now
-    """
-    parsed = parse_spmw_module(module, top_name)
-
-    mod = parsed["module"]
-    symbol_map = parsed["symbols"]
-    work_grids = parsed["grids"]
-    dtensors = parsed["dtensors"]
-    instance_copies = {}
-
-    # construct raw function copies
-    # TODO: reuse copies if they have same control flow
-    with module.context, Location.unknown():
-        copies = Module.create()
-        with InsertionPoint(copies.body):
-            for op in mod.body.operations:
-                if is_resource(op):
-                    op.clone()
-            for orig_func_name, grid_shape in work_grids.items():
-                orig_func = symbol_map[orig_func_name]
-                for dim in np.ndindex(*grid_shape):
-                    func_ = orig_func.clone()
-                    function_name = construct_name(orig_func_name, dim)
-                    func_.sym_name = StringAttr.get(function_name)
-                    instance_copies[function_name] = func_
-                    # meta data
-                    for func_block in func_.body:
-                        for op in func_block.operations:
-                            assert isinstance(op, func_d.CallOp)
-                            assert op.callee.value.endswith("get_wid")  # FIXME: parsing
-                            for res, num in zip(op.results, dim):
-                                const_op = arith_d.ConstantOp(
-                                    mlir_types.index(), num, ip=InsertionPoint(op)
-                                )
-                                res.replace_all_uses_with(const_op.result)
-                            op.erase()
-                            break
-                        break
-
-        unroll_meta_for(copies)
-        pipeline = "builtin.module(lower-memcopy-ops, canonicalize)"
-        mlir_pass_manager.parse(pipeline).run(copies.operation)
-        replace_stream_arrays(copies)
-
-    # construct top module
-    top_module = symbol_map[top_name]
-    resources = {}
-    for k, v in symbol_map.items():
-        if k.startswith(top_name) and is_resource(v):
-            resources[k] = v
-
-    new_resources = {}
-    with InsertionPoint(mod.body), Location.unknown():
-        top_func = func_d.FuncOp(top_name, top_module.type)
-        entry_block = top_func.add_entry_block()
-        attr.copy_attr(top_module, top_func, {"itypes", "otypes"}, allow_missing=False)
-        entry_ip = InsertionPoint.at_block_begin(entry_block)
-        top_func.attributes["dataflow"] = UnitAttr.get()
-
-        # move resource to local
-        with entry_ip:
-            for name, op in resources.items():
-                if isinstance(op, allo_d.StreamGlobalOp):
-                    shape = list(op.shape)
-                    if len(shape) == 0:
-                        names = [name]
-                    else:
-                        names = [
-                            construct_name(name, dim) for dim in np.ndindex(*shape)
-                        ]
-                    for n in names:
-                        stream = allo_d.StreamConstructOp(op.element_type.value)
-                        new_resources[n] = stream.result
-                else:
-                    raise NotImplementedError
-
-    with InsertionPoint.at_block_begin(mod.body), Location.unknown():
-        # call ops
-        for orig_func_name, grid_shape in work_grids.items():
-            tensors = dtensors[orig_func_name]  # argument passing
-            for dim in np.ndindex(*grid_shape):
-                function_name = construct_name(orig_func_name, dim)
-                instance = instance_copies[function_name]
-                # [NOTE]: backend do not support `.` in function names, backend use `startswith` to identify top module
-                hls_function_name = f"_{function_name.replace(".", "_")}"
-                symbols, _ = collect_symbol_refs_in_function(instance)
-                symbol_names = sorted(symbols.keys())
-                itypes = instance.type.inputs
-                base_inputs = len(itypes)
-                assert "itypes" in instance.attributes
-                itype_hints = instance.attributes["itypes"].value
-                args = []
-                for t in tensors:
-                    assert t.tile_shape == t.shape  # hls do not support sharding
-                    args.append(top_func.arguments[t.global_id])
-                for symbol in symbol_names:
-                    resource_op = new_resources[symbol]
-                    args.append(resource_op)
-                    itypes.append(resource_op.type)
-                    itype_hints += "_"  # FIXME
-                new_func = func.function(
-                    hls_function_name,
-                    itypes,
-                    [],
-                    itype_hints=itype_hints,
-                    otype_hints="",
-                )
-                final_op = func_d.ReturnOp([], ip=InsertionPoint(new_func.entry_block))
-                # update arguments
-                for i, arg in enumerate(instance.arguments):
-                    arg.replace_all_uses_with(new_func.arguments[i])
-                for op in instance.entry_block.operations:
-                    op.operation.move_before(final_op)
-                final_op.erase()
-                # update resource usage
-                for i, symbol in enumerate(symbol_names):
-                    ops = symbols[symbol]
-                    for op in ops:
-                        if isinstance(op, allo_d.GlobalStreamGetOp):
-                            new_op = allo_d.StreamGetOp(
-                                op.result.type,
-                                new_func.arguments[i + base_inputs],
-                                [],
-                                ip=InsertionPoint(op),
-                            )
-                            op.result.replace_all_uses_with(new_op.result)
-                            op.erase()
-                        elif isinstance(op, allo_d.GlobalStreamPutOp):
-                            allo_d.StreamPutOp(
-                                new_func.arguments[i + base_inputs],
-                                [],
-                                op.data,
-                                ip=InsertionPoint(op),
-                            )
-                            op.erase()
-                        else:
-                            print(op)
-                            raise NotImplementedError
-                # call the function in top module
-                func_d.CallOp(
-                    [],
-                    FlatSymbolRefAttr.get(hls_function_name),
-                    args,
-                    ip=entry_ip,
-                )
-        func_d.ReturnOp([], ip=entry_ip)
-
-    for op in mod.body.operations:
-        if isinstance(op, allo_d.StreamGlobalOp):
-            op.erase()
-
-    return mod
-
-
-def instantiate_for_hierarchical_hls(module, top_name):
     parsed = parse_hierarchical_spmw_module(module)
 
     mod = parsed["module"]
--- tests/test_hls_hierarchical.py
+++ tests/test_hls_hierarchical.py
@@ -6,11 +6,11 @@ import allo.backend.hls as hls
 from src.hls import to_hls
 import tempfile
 import allo
-from allo.ir.types import int32
+from allo.ir.types import int32, float32, ConstExpr
 from allo import spmw
 
 
-def test():
+def test1():
     @spmw.unit()
     def vadd(A: int32[1024], B: int32[1024]):
         @spmw.work(mapping=[1])
@@ -30,12 +30,51 @@ def test():
             A = np.random.randint(0, 100, (1024,), dtype=np.int32)
             B = np.random.randint(0, 100, (1024,), dtype=np.int32)
             C = np.zeros((1024,), dtype=np.int32)
-            mod = to_hls(top, project=tmpdir)
+            mod = to_hls(top)
             mod(A, A, B, C)
             # assert fail due to backend issues
             # np.testing.assert_allclose(A + 1, B)
             # np.testing.assert_allclose(A + 1, C)
 
 
+def test_hierachical_function():
+    M, N, K = 32, 32, 32
+    P0, P1 = 2, 2
+
+    @spmw.unit()
+    def inner(A: float32[M, K], B: float32[K, N], C: float32[M, N]):
+        @spmw.work(mapping=[P0, P1])
+        def gemm():
+            pi, pj = spmw.get_wid()
+            Mt: ConstExpr[int32] = M // P0
+            Nt: ConstExpr[int32] = N // P1
+            for i in range(pi * Mt, (pi + 1) * Mt):
+                for j in range(pj * Nt, (pj + 1) * Nt):
+                    for k in range(K):
+                        C[i, j] += A[i, k] * B[k, j]
+
+    @spmw.unit()
+    def top(A: float32[M, K], B: float32[K, N], C1: float32[M, N], C2: float32[M, N]):
+        @spmw.work(mapping=[2])
+        def wrapper():
+            i = spmw.get_wid()
+            if i == 0:
+                inner(A, B, C1)
+            else:
+                inner(A, B, C2)
+
+    if hls.is_available("vitis_hls"):
+        A = np.random.rand(M, K).astype(np.float32)
+        B = np.random.rand(K, N).astype(np.float32)
+        C1 = np.zeros((M, N), dtype=np.float32)
+        C2 = np.zeros((M, N), dtype=np.float32)
+        mod = to_hls(top)
+        mod(A, B, C1, C2)
+        np.testing.assert_allclose(C1, np.dot(A, B), rtol=1e-5, atol=1e-5)
+        np.testing.assert_allclose(C2, np.dot(A, B), rtol=1e-5, atol=1e-5)
+        print("Success!")
+
+
 if __name__ == "__main__":
-    test()
+    # test1()
+    test_hierachical_function()
